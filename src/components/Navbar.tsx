31c8fb6bfe3556eefd4c2e379f863827581f6a05
AmandaFerreira-prog <103228991+AmandaFerreira-prog@users.noreply.github.com>
2025-01-02 13:40:45 -0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar({ activeButton }: NavbarProps) {
                         )}
                     />
                 </Link>
-                <Link href="https://github.com/exoonero/extrator" target="_blank">
+                <Link href="https://github.com/AmandaFerreira-prog/extrator_licita" target="_blank">
                     <GithubIcon className="fill-[#152544] hover:fill-gray-600 w-8 h-auto" />
                 </Link>
             </div>
